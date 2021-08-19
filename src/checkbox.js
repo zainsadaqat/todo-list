15df15060c0fa9f2664d694ef565aae67bb8270b
Zain Sadaqat <83048208+zainsadaqat@users.noreply.github.com>
2021-08-19 23:16:37 +0500

    Delete unnecessary files

--- src/checkbox.js
+++
@@ -1,8 +0,0 @@
-// Access checkbox
-export const checkBoxField = document.querySelector(
-  '[data-list-item-checkbox]'
-);
-
-checkBoxField.addEventListener('change', () => {
-  console.log('Checked ...');
-});
--- src/todo.js
+++
@@ -1,7 +0,0 @@
-export default class Todo {
-  constructor(index, description, completed) {
-    this.index = index;
-    this.description = description;
-    this.completed = completed;
-  }
-}
