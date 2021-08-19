ffb9696e4d9dd270af55ad346f5e01cff91b9980
Zain Sadaqat <83048208+zainsadaqat@users.noreply.github.com>
2021-08-20 01:23:19 +0500

    Create modules

--- src/index.js
+++ src/index.js
@@ -1,30 +1,44 @@
+import Todo from './todo';
+import { checkBoxEvent } from './checkbox';
 import './style.css';
 
 const TODO_LIST_KEY = 'TODO_LIST_KEY';
 
-const todoList = JSON.parse(localStorage.getItem(TODO_LIST_KEY)) || [];
+const loadList = () => {
+  let dataInStringFormat = localStorage.getItem(TODO_LIST_KEY);
+  return JSON.parse(dataInStringFormat) || [];
+};
+
+const todoList = loadList();
+
+const saveList = () => {
+  localStorage.setItem(TODO_LIST_KEY, JSON.stringify(todoList));
+};
 
 const todoListContainer = document.querySelector('#list');
 const form = document.querySelector('.form');
+const inputField = document.querySelector('#todo-input');
+
+const clearField = () => {
+  inputField.value = '';
+};
 
 form.addEventListener('submit', () => {
-  const inputField = document.querySelector('#todo-input');
+  // Access Template Elements
   const template = document.querySelector('#list-item-template');
   const templateClone = template.content.cloneNode(true);
   const taskContent = templateClone.querySelector('[data-list-item-text]');
   const checkBox = templateClone.querySelector('[data-list-item-checkbox]');
 
-  const obj = {
-    description: inputField.value,
-    completed: false,
-    index: todoList.length,
-  };
-  todoList.push(obj);
-  localStorage.setItem(TODO_LIST_KEY, JSON.stringify(todoList));
+  // Create a new Todo
+  const todoTemplate = new Todo(todoList.length, inputField.value, false);
+  todoList.push(todoTemplate);
+  saveList();
   taskContent.innerText = inputField.value;
   checkBox.checked = checkBox.checked ? true : false;
   todoListContainer.appendChild(templateClone);
   console.log(todoList);
+  clearField();
 });
 
 todoList.forEach((todo) => {
@@ -33,21 +47,12 @@ todoList.forEach((todo) => {
   const taskContent = templateClone.querySelector('[data-list-item-text]');
   const checkBox = templateClone.querySelector('[data-list-item-checkbox]');
   checkBox.addEventListener('change', (e) => {
-    console.log('Index: ', todo.index);
-    if (checkBox.checked) {
-      taskContent.classList.add('line-through');
-      todo.completed = checkBox.checked;
-      localStorage.setItem(TODO_LIST_KEY, JSON.stringify(todoList));
-      console.log('Checkbox Completed: ', todo.completed);
-    } else {
-      taskContent.classList.remove('line-through');
-      todo.completed = checkBox.checked;
-      localStorage.setItem(TODO_LIST_KEY, JSON.stringify(todoList));
-      console.log('Checkbox Completed: ', todo.completed);
-    }
+    // Save checkbox event in local storage
+    checkBoxEvent(todo, checkBox, taskContent, saveList);
   });
   taskContent.innerText = todo.description;
   checkBox.checked = todo.completed;
+  saveList();
   if (checkBox.checked) {
     taskContent.classList.add('line-through');
   } else {
